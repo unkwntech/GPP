ee8e01839e4ed03465b173f2e033181f3f99521e
Arron Chapman <unkwntech@gmail.com>
2024-11-21 17:59:09 -0600

    Fix a but with the region name when generating dotlan links for routes

--- src/commands/route.command.ts
+++ src/commands/route.command.ts
@@ -41,15 +41,17 @@ export default {
                         shortcut = true;
                         output += `through **Thera** via the entrance at [${
                             data.route[i - 1].system_name
-                        }](https://evemaps.dotlan.net/map/${
-                            data.route[i - 1].region_name
-                        }/${
+                        }](https://evemaps.dotlan.net/map/${data.route[
+                            i - 1
+                        ].region_name.replaceAll(" ", "_")}/${
                             data.route[i - 1].system_name
                         }), you will exit too [${
                             data.route[i - 1].system_name
-                        }](https://evemaps.dotlan.net/map/${
-                            data.route[i + 1].region_name
-                        }/${data.route[i + 1].system_name}).`;
+                        }](https://evemaps.dotlan.net/map/${data.route[
+                            i + 1
+                        ].region_name.replaceAll(" ", "_")}/${
+                            data.route[i + 1].system_name
+                        }).`;
                         break;
                     }
                     if (system.system_name === "Turnur") {
