7186e2e014d619aababa849e737e5f74be21b821
Arron Chapman <unkwntech@gmail.com>
2024-11-20 19:14:31 -0600

    Removed sql server connection from gif command

--- src/commands/rats.commands.ts
+++ src/commands/rats.commands.ts
@@ -6,12 +6,6 @@ export default {
         .setName("rats")
         .setDescription("A random Rat gif to bring you fulfillment"),
     execute: async (interaction: ChatInputCommandInteraction) => {
-        if (interaction.client.mysql.state !== "connected") {
-            interaction.client.mysql.connect((err) => {
-                console.info("MYSQL CONNECTED");
-            });
-        }
-
         axios
             .get(
                 `https://tenor.googleapis.com/v2/search?q=rat&key=${process.env.TENOR_API_KEY}&client_key=discord&limit=1&media_filter=gif&random=true`
