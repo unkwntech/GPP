f99c7d639a29bde9493e59a88911b16459300453
Arron Chapman <unkwntech@gmail.com>
2025-01-18 19:57:46 -0600

    updated git path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
             host: "ibns.tech",
             key: "deploy.key",
             ref: "origin/main",
-            repo: "git@github.com:unkwntech/GPP",
+            repo: "git@github.com:unkwntech/GPP.git",
             path: "/var/projects/cheesus-backend-prod/",
             "post-deploy":
                 "npm i && tsc -b && pm2 reload ecosystem.config.js --env production && pm2 save",
